4951c0f82a3dba9a41308ed4d45ce7e9cca0c49c
David Horstman <davidzilla27@gmail.com>
2020-05-24 21:21:57 -0400

    Remaining methods in model

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -12,8 +12,43 @@ function findById(id) {
 
 function findSteps(id) {
   return db("steps")
-    .where({ "scheme_id": id })
+    .where({ scheme_id: id })
     .join("schemes", "steps.scheme_id", "schemes.id")
-    .select("steps.id", " schemes.scheme_name", " steps.step_number", " steps.instructions");
+    .select(
+      "steps.id",
+      " schemes.scheme_name",
+      " steps.step_number",
+      " steps.instructions"
+    );
 }
-module.exports = { find, findById, findSteps };
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, ["id"])
+    .then(ids => {
+      const id = ids[0].id || ids[0];
+      return findById(id);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .update(changes)
+    .where({ id })
+    .then(count => {
+      if (count == 1) return findById(id);
+      throw new Error("Error updating the record");
+    });
+}
+
+async function remove(id) {
+  const entry = await findById(id);
+  return db("schemes")
+    .where({ id })
+    .delete()
+    .then(count => {
+      if (count == 1) return entry;
+      return null;
+    });
+}
+module.exports = { find, findById, findSteps, add, update, remove };
